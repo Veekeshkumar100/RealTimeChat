4832ccc59fc22ce214be9eaa62fef96f38cbfe02
Veekeshkumar100 <veekesh487@gmail.com>
2025-10-09 16:37:01 +0530

    first commit

--- client/react/src/App.jsx
+++ client/react/src/App.jsx
@@ -4,8 +4,15 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Login from './page/Authentication/Login';
 import SignUp from './page/Authentication/signUp';
 import Home from './page/Home/Home';
+import  {useSelectore} from 'react-redux'
 
-function App() {
+
+
+function App(){
+   const reduxdata= useSelectore((state)=state);
+  console.log(reduxdata);
+
+
  const router=createBrowserRouter([
   {
     path:"/",
